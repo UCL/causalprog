c540fc8ce1e6d788ae3c403d02f722ed590cd193
willGraham01 <1willgraham@gmail.com>
2025-03-06 12:10:23 +0000

    Distributions sample method is backend-agnostic

--- src/causalprog/distributions/base.py
+++ src/causalprog/distributions/base.py
@@ -3,6 +3,10 @@
 from collections.abc import Callable
 from typing import Generic, ParamSpec, TypeVar
 
+from numpy.typing import ArrayLike
+
+RNGKey = TypeVar("RNGKey")
+SupportsRNG = TypeVar("SupportsRNG")
 SupportsSampling = TypeVar("SupportsSampling")
 P = ParamSpec("P")
 
@@ -11,13 +15,38 @@ class Distribution(Generic[SupportsSampling]):
     """Distributions."""
 
     _dist: SupportsSampling
+    _backend_sample_info: dict[
+        str, str
+    ]  # Should probably make immutable, or into a namedtuple, for example
 
-    def __init__(self, backend_distribution: SupportsSampling) -> None:
+    @property
+    def _sample(self) -> Callable[[RNGKey, ArrayLike], ArrayLike]:
+        backend_sample_method = getattr(self._dist, self._backend_sample_info["method"])
+        return lambda key, sample_size: backend_sample_method(
+            **{
+                self._backend_sample_info["key"]: key,
+                self._backend_sample_info["sample_size"]: sample_size,
+            }
+        )
+
+    def __init__(
+        self,
+        backend_distribution: SupportsSampling,
+        backend_sample_function: str = "sample",
+        backend_sample_key_arg: str = "key",
+        backend_sample_size_arg: str = "sample_size",
+    ) -> None:
         """Create a new Distribution."""
         self._dist = backend_distribution
+        self._backend_sample_info = {
+            "method": backend_sample_function,
+            "key": backend_sample_key_arg,
+            "sample_size": backend_sample_size_arg,
+        }
 
-    def sample(self, *args, **kwargs) -> None:  # noqa: ANN002, ANN003
+    def sample(self, key: SupportsRNG, sample_size: ArrayLike = ()) -> None:
         """Draw samples from the distribution."""
+        return self._sample(key, sample_size)
 
 
 class DistributionFamily(Generic[SupportsSampling, P]):
