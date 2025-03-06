f69e4f5a702ecccf79bf6d9c43ce0bbda65ff725
willGraham01 <1willgraham@gmail.com>
2025-03-06 11:03:08 +0000

    Base class for distribution families

--- src/causalprog/distributions/base.py
+++ src/causalprog/distributions/base.py
@@ -1,12 +1,13 @@
 """Base class for backend-agnostic distributions."""
 
-from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from collections.abc import Callable
+from typing import Generic, ParamSpec, TypeVar
 
 SupportsSampling = TypeVar("SupportsSampling")
+P = ParamSpec("P")
 
 
-class Distribution(ABC, Generic[SupportsSampling]):
+class Distribution(Generic[SupportsSampling]):
     """Distributions."""
 
     _dist: SupportsSampling
@@ -15,6 +16,23 @@ class Distribution(ABC, Generic[SupportsSampling]):
         """Create a new Distribution."""
         self._dist = backend_distribution
 
-    @abstractmethod
     def sample(self, *args, **kwargs) -> None:  # noqa: ANN002, ANN003
         """Draw samples from the distribution."""
+
+
+class DistributionFamily(Generic[SupportsSampling, P]):
+    """
+    A family of distributions, specified by common parameters.
+
+    Essentially a factory class for `Distribution`s.
+    """
+
+    _constructor: Callable[P, SupportsSampling]
+
+    def __init__(self, distribution_constructor: Callable[P, SupportsSampling]) -> None:
+        """Specify a family of Distributions."""
+        self._constructor = distribution_constructor
+
+    def __call__(self, *args, **kwargs) -> Distribution:  # noqa: ANN002, ANN003
+        """Create a Distribution from parameters."""
+        return Distribution(self._constructor(*args, **kwargs))
