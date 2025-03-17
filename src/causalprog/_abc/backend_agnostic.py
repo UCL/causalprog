3dd33457286b2e22ad630c850fe46314f81628c0
willGraham01 <1willgraham@gmail.com>
2025-03-17 14:12:43 +0000

    Create Translator class, now very much backend agnostic

--- src/causalprog/_abc/backend_agnostic.py
+++ src/causalprog/_abc/backend_agnostic.py
@@ -1,10 +1,26 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+Backend = TypeVar("Backend")
 
 
-class BackendAgnostic(ABC):
+class BackendAgnostic(ABC, Generic[Backend]):
     """A frontend object that must be backend-agnostic."""
 
-    _backend_obj: object
+    _backend_obj: Backend
 
-    def __init__(self, *, backend: object) -> None:
+    def __init__(self, *, backend: Backend) -> None:
         self._backend_obj = backend
+
+    @property
+    @abstractmethod
+    def _frontend_provides(self) -> tuple[str, ...]:
+        """Methods that the frontend object must provide."""
+
+    def _validate(self) -> bool:
+        """Return ``True`` if all ``_frontend_provides`` methods are present."""
+        return all(hasattr(self, attr) for attr in self._frontend_provides)
+
+    def get_backend(self) -> Backend:
+        """Access to the backend object."""
+        return self._backend_obj
--- src/causalprog/backend/translator.py
+++ src/causalprog/backend/translator.py
@@ -3,6 +3,9 @@
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from inspect import Signature
+from typing import Any
+
+from causalprog._abc.backend_agnostic import Backend, BackendAgnostic
 
 from ._typing import ParamNameMap, ReturnType, StaticValues
 from .signature import convert_signature
@@ -10,15 +13,24 @@ from .signature import convert_signature
 
 @dataclass
 class Translation:
-    """Helper class for mapping frontend signatures to backend signatures."""
+    """
+    Helper class for mapping frontend signatures to backend signatures.
+
+    Predominantly a convenience wrapper for working with different backends.
+    The attributes stored in an instance form the compulsory arguments that
+    need to be passed to ``convert_signature`` in order to map a backend
+    function to the frontend syntax.
+    """
 
     target_signature: Signature
     param_map: ParamNameMap
     frozen_args: StaticValues = field(default_factory=dict)
+    target_name: str | None = None
 
     def __post_init__(self) -> None:
         self.param_map = dict(self.param_map)
         self.frozen_args = dict(self.frozen_args)
+        self.target_name = str(self.target_name) if self.target_name else None
 
         if not all(
             isinstance(key, str) and isinstance(value, str)
@@ -37,11 +49,84 @@ class Translation:
         )
 
 
-class Translator:
-    """Translates the methods of a backend object into frontend syntax."""
+class Translator(BackendAgnostic[Backend]):
+    """
+    Translates the methods of a backend object into frontend syntax.
 
-    known_translations: dict[str, Translation]
-    target_class: object
+    A ``Translator`` acts as an intermediary between a backend that is supplied by the
+    user and the frontend syntax that ``causalprog`` relies on. The default backend of
+    ``causalprog`` uses a syntax compatible with ``jax``.
 
-    def translate(self, backend_obj: object):
-        pass
+    Other backends may not conform to the syntax that ``causalprog`` expects, but
+    nonetheless may provide the functionality that it requires. A ``Translator`` is able
+    to make calls to (the relevant methods of) this backend, whilst still conforming to
+    the frontend syntax of ``causalprog``.
+
+    As an example, suppose that we have a frontend class ``C`` that needs to provide a
+    method ``do_this``. ``causalprog`` expects ``C`` to provide the functionality
+    of ``do_this`` via one of its methods, ``C.do_this(*c_args, **c_kwargs)``.
+    Now suppose that a class ``D`` from a different, external package might also
+    provides the functionality of ``do_this``, but it is done by calling
+    ``D.do_this_different(*d_args, **d_kwargs)``, where there is some mapping
+    ``m: *c_args, **c_kwargs -> *d_args, **d_kwargs``. In such a case, ``causalprog``
+    needs to use a ``Translator`` ``T``, rather than ``D`` directly, where
+
+    ``T.do_this(*c_args, **c_kwargs) = D.do_this_different(m(*c_args, **c_kwargs))``.
+    """
+
+    translations: dict[str, Callable]
+
+    def __init__(
+        self,
+        native: Backend,
+        **known_translations: Translation,
+    ) -> None:
+        """
+        Translate a backend object into a frontend-compatible object.
+
+        Args:
+            native (Backend): Backend object that must be translated to support frontend
+                syntax.
+            **known_translations (Translation): Keyword-specified ``Translation``
+                objects that map the methods of ``native`` to the (signatures of the)
+                methods that the ``_frontend_provides``. Keyword names are interpreted
+                as the name of the backend method to translate, whilst
+                ``Translation.target_name`` is interpreted as the name of the frontend
+                method that this backend method performs the role of.
+
+        """
+        super().__init__(backend=native)
+
+        self.translations = {}
+        for native_name, t in known_translations.items():
+            translated_name = t.target_name if t.target_name else native_name
+            native_method = getattr(self._backend_obj, native_name)
+
+            if translated_name in self.translations:
+                msg = f"Method {translated_name} provided twice."
+                raise ValueError(msg)
+            self.translations[translated_name] = convert_signature(
+                native_method, t.target_signature, t.param_map, t.frozen_args
+            )
+        if not self._validate():
+            # Some methods must not change name.
+            missing = {
+                name
+                for name in self._frontend_provides
+                if name not in self.translations
+            }
+            for missing_method in missing:
+                if hasattr(self._backend_obj, missing_method):
+                    self.translations[missing_method] = getattr(
+                        self._backend_obj, missing_method
+                    )
+                else:
+                    msg = f"Compulsory method {missing_method} not provided."
+                    raise ValueError(msg)
+
+    def __getattr__(self, name: str) -> Any:  # noqa: ANN401
+        """Get a (possibly translated) method from this Translator."""
+        if name in self.translations:
+            return self.translations[name]
+        msg = f"{self} has no attribute {name}."
+        raise AttributeError(msg)
