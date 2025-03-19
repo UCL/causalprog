e460f4fead0b211911bbd7e5d64f5199ecc4e643
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-03-19 11:12:03 +0000

    Update src/causalprog/graph/graph.py
    Co-authored-by: Will Graham <32364977+willGraham01@users.noreply.github.com>

--- src/causalprog/graph/graph.py
+++ src/causalprog/graph/graph.py
@@ -29,7 +29,7 @@ class Graph(Labelled):
 
     def add_node(self, node: Node) -> None:
         """Add a node to the graph."""
-        if node._label is None:  # noqa: SLF001
+        if node.label is None:
             while f"node{self._node_index}" in self._nodes_by_label:
                 self._node_index += 1
             node._label = f"node{self._node_index}"  # noqa: SLF001
