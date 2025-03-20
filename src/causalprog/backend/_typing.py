5b2dbae4d2f576013ecd253d798067e7dda09373
willGraham01 <1willgraham@gmail.com>
2025-03-20 11:09:03 +0000

    Hide hidden variable import behind typehint

--- src/causalprog/backend/_typing.py
+++ src/causalprog/backend/_typing.py
@@ -1,5 +1,7 @@
+from inspect import _ParameterKind
 from typing import Any, TypeAlias, TypeVar
 
 ReturnType = TypeVar("ReturnType")
 ParamNameMap: TypeAlias = dict[str, str]
+ParamKind: TypeAlias = _ParameterKind
 StaticValues: TypeAlias = dict[str, Any]
