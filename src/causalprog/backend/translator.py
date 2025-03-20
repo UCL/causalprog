24e7e05e449b35860c9b6d6d470781129963e339
willGraham01 <1willgraham@gmail.com>
2025-03-20 15:28:47 +0000

    Hidden method for calling the backend

--- src/causalprog/backend/translator.py
+++ src/causalprog/backend/translator.py
@@ -2,15 +2,16 @@
 
 from collections.abc import Callable
 from dataclasses import dataclass, field
-from inspect import Signature
+from inspect import signature
 from typing import Any
 
 from causalprog._abc.backend_agnostic import Backend, BackendAgnostic
 
 from ._convert_signature import convert_signature
-from ._typing import ParamNameMap, ReturnType, StaticValues
+from ._typing import ParamNameMap, StaticValues
 
 
+# TODO: Tests for this guy
 @dataclass
 class Translation:
     """
@@ -22,15 +23,16 @@ class Translation:
     function to the frontend syntax.
     """
 
-    target_signature: Signature
+    backend_name: str
+    frontend_name: str
     param_map: ParamNameMap
     frozen_args: StaticValues = field(default_factory=dict)
-    target_name: str | None = None
 
     def __post_init__(self) -> None:
-        self.param_map = dict(self.param_map)
+        self.backend_name = str(self.backend_name)
+        self.frontend_name = str(self.frontend_name)
         self.frozen_args = dict(self.frozen_args)
-        self.target_name = str(self.target_name) if self.target_name else None
+        self.param_map = dict(self.param_map)
 
         if not all(
             isinstance(key, str) and isinstance(value, str)
@@ -42,13 +44,8 @@ class Translation:
             msg = "Frozen args must be specified by name (str)"
             raise ValueError(msg)
 
-    def translate(self, fn: Callable[..., ReturnType]) -> Callable[..., ReturnType]:
-        """Convert a (compatible) callable's signature into the target_signature."""
-        return convert_signature(
-            fn, self.target_signature, self.param_map, self.frozen_args
-        )
 
-
+# TODO: tests for this guy after tests for the above guy!
 class Translator(BackendAgnostic[Backend]):
     """
     Translates the methods of a backend object into frontend syntax.
@@ -74,17 +71,18 @@ class Translator(BackendAgnostic[Backend]):
     ``T.do_this(*c_args, **c_kwargs) = D.do_this_different(m(*c_args, **c_kwargs))``.
     """
 
+    frontend_to_native_names: dict[str, str]
     translations: dict[str, Callable]
 
     @staticmethod
-    def identity(*args: Any, **kwargs: Any) -> tuple[tuple, dict[str, Any]]:  # noqa: ANN401
+    def identity(*args: Any, **kwargs: Any) -> tuple[tuple[Any, ...], dict[str, Any]]:  # noqa: ANN401
         """Identity map on positional and keyword arguments."""
         return args, kwargs
 
     def __init__(
         self,
         native: Backend,
-        **translations: Translation,
+        *translations: Translation,
     ) -> None:
         """
         Translate a backend object into a frontend-compatible object.
@@ -103,16 +101,17 @@ class Translator(BackendAgnostic[Backend]):
         super().__init__(backend=native)
 
         self.translations = {}
-        for native_name, t in translations.items():
-            translated_name = t.target_name if t.target_name else native_name
+        self.frontend_to_native_names = {name: name for name in self._frontend_provides}
+        for t in translations:
+            native_name = t.backend_name
+            translated_name = t.frontend_name
             native_method = getattr(self._backend_obj, native_name)
+            target_signature = signature(getattr(self, translated_name))
 
-            if translated_name in self.translations:
-                msg = f"Method {translated_name} provided twice."
-                raise ValueError(msg)
             self.translations[translated_name] = convert_signature(
-                native_method, t.target_signature, t.param_map, t.frozen_args
+                native_method, target_signature, t.param_map, t.frozen_args
             )
+            self.frontend_to_native_names[translated_name] = native_name
 
         # Methods without explicit translations are assumed to be the identity map
         for method in self._frontend_provides:
@@ -121,8 +120,12 @@ class Translator(BackendAgnostic[Backend]):
 
         self.validate()
 
-    def __getattr__(self, name: str) -> Any:  # noqa: ANN401
-        # Check for translations before falling back on backend directly.
-        if name in self.translations:
-            return self.translations[name]
-        return super().__getattr__(name)
+    def _call_backend_with(self, method: str, *args: Any, **kwargs: Any) -> Any:  # noqa:ANN401
+        """Translate arguments and then call the backend."""
+        backend_method = getattr(self._backend_obj, method)
+        backend_args, backend_kwargs = self.translations[method](*args, **kwargs)
+        return backend_method(*backend_args, **backend_kwargs)
+
+    # IDEA NOW is that we could now define
+    # def sample(*args, **kwargs):
+    #    return self._call_backend_with("sample", *args, **kwargs)
