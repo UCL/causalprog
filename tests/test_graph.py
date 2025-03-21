f6e69f90bdcc815d38cbf69b9bea4c096c565817
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-03-21 16:56:16 +0000

    noqa

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -202,7 +202,7 @@ def test_single_normal_node(samples, rtol, mean, stdev, rng_key):
         ),
     ],
 )
-def test_two_node_graph(samples, rtol, mean, stdev, stdev2, rng_key):  # noqa: PLR0913
+def test_two_node_graph(samples, rtol, mean, stdev, stdev2, rng_key):
     if samples > 100:  # noqa: PLR2004
         pytest.xfail("Test currently too slow")
     graph = causalprog.graph.Graph("G0")
