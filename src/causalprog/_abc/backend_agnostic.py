65e26ee5286141bc22ee071d821bef728b4cdbe6
Will Graham <32364977+willGraham01@users.noreply.github.com>
2025-03-21 09:13:07 +0000

    Backend-agnostic ABC (#28)
    * Pull backend_agnostic class
    
    * Pull backend_agnostic tests
    
    * Test positive and negative case
    
    * Tidy docstrings and methods

---
+++ src/causalprog/_abc/backend_agnostic.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from typing import Any, Generic, TypeVar
+
+Backend = TypeVar("Backend")
+
+
+class BackendAgnostic(ABC, Generic[Backend]):
+    """
+    A frontend object that must be backend-agnostic.
+
+    ``BackendAgnostic`` is a means of ensuring that an object provides the functionality
+    and interface that our package expects, irrespective of how this functionality is
+    actually carried out. An instance of a ``BackendAgnostic`` class stores a reference
+    to its ``_backend_obj``, and falls back on this object's methods and attributes if
+    the instance itself does not possess the required attributes. Methods that the
+    ``BackendAgnostic`` object can also be explicitly defined in the class, and make
+    calls to the ``_backend_obj`` as necessary.
+    """
+
+    __slots__ = ("_backend_obj",)
+    _backend_obj: Backend
+
+    def __getattr__(self, name: str) -> Any:  # noqa: ANN401
+        """Fallback on the ``_backend_obj`` a frontend attribute isn't found."""
+        if name in self._frontend_provides and hasattr(self._backend_obj, name):
+            return getattr(self._backend_obj, name)
+        msg = f"{self} has no attribute {name}."
+        raise AttributeError(msg)
+
+    def __init__(self, *, backend: Backend) -> None:
+        self._backend_obj = backend
+
+    @property
+    @abstractmethod
+    def _frontend_provides(self) -> tuple[str, ...]:
+        """Names of attributes that an instance of this class must provide."""
+
+    @property
+    def _missing_attrs(self) -> set[str]:
+        """Return the names of frontend attributes that are missing."""
+        return {attr for attr in self._frontend_provides if not hasattr(self, attr)}
+
+    def get_backend(self) -> Backend:
+        """Access to the backend object."""
+        return self._backend_obj
+
+    def validate(self) -> None:
+        """
+        Determine if all expected frontend attributes are provided.
+
+        Raises:
+            AttributeError: If frontend methods are not present.
+
+        """
+        if len(self._missing_attrs) != 0:
+            raise AttributeError(
+                "Missing frontend methods: " + ", ".join(self._missing_attrs)
+            )
---
+++ tests/test__abc/test_backend_agnostic.py
@@ -0,0 +1,72 @@
+import re
+
+import pytest
+
+from causalprog._abc.backend_agnostic import BackendAgnostic
+
+
+class OneMethodBackend:
+    def method1(self) -> None:
+        return
+
+
+class TwoMethodBackend(OneMethodBackend):
+    def method2(self) -> None:
+        return
+
+
+class ThreeMethodBackend(TwoMethodBackend):
+    def method3(self) -> None:
+        return
+
+
+class BA(BackendAgnostic):
+    """
+    Designed to test the abstract ``BackendAgnostic`` class.
+
+    Instances take ``*methods`` as an argument, which has the effect of setting
+    ``self.method`` to be a function that returns ``True`` for each ``method`` in
+    ``*methods``.
+    """
+
+    @property
+    def _frontend_provides(self) -> tuple[str, ...]:
+        return (
+            "method1",
+            "method2",
+        )
+
+
+@pytest.mark.parametrize(
+    ("backend", "expected_missing"),
+    [
+        pytest.param(
+            TwoMethodBackend(),
+            set(),
+            id="All methods defined.",
+        ),
+        pytest.param(
+            ThreeMethodBackend(),
+            set(),
+            id="Additional methods defined.",
+        ),
+        pytest.param(
+            OneMethodBackend(),
+            {"method2"},
+            id="Missing required method.",
+        ),
+    ],
+)
+def test_method_discovery(backend: object, expected_missing: set[str]) -> None:
+    obj = BA(backend=backend)
+    assert obj.get_backend() is backend
+
+    assert obj._missing_attrs == expected_missing  # noqa: SLF001
+    if len(expected_missing) != 0:
+        with pytest.raises(
+            AttributeError,
+            match=re.escape("Missing frontend methods: " + ", ".join(expected_missing)),
+        ):
+            obj.validate()
+    else:
+        obj.validate()
