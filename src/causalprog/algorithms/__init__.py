f4f625a07374bef78e6a69b0403d87dc7704c2dc
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-04-30 15:13:45 +0100

    make do work, ruff

--- src/causalprog/algorithms/__init__.py
+++ src/causalprog/algorithms/__init__.py
@@ -1,4 +1,4 @@
 """Algorithms."""
 
-from .expectation import expectation, standard_deviation
 from .do import do
+from .expectation import expectation, standard_deviation
--- src/causalprog/algorithms/do.py
+++ src/causalprog/algorithms/do.py
@@ -1,21 +1,32 @@
 """Algorithms for applying do to a graph."""
 
-import numpy.typing as npt
-
 from causalprog.graph import Graph, ParameterNode
 
 
 def do(graph: Graph, node: str, value: float, label: str | None = None) -> Graph:
+    """
+    Apply do to a graph.
+
+    Args:
+        graph: The graph to apply do to. This will be copied.
+        node: The label of the node to apply do to.
+        value: The value to set the node to.
+        label: The label of the new graph
+
+    Returns:
+        A copy of the graph with do applied
+
+    """
     if label is None:
         label = f"{graph.label}|do({node}={value})"
 
-    g = graph._graph.copy()
+    old_g = graph._graph  # noqa: SLF001
+    g = old_g.copy()
 
     new_node = ParameterNode(node, value=value)
     g.add_node(new_node)
 
-    edges_to_remove = []
-    for e in graph._graph.edges:
+    for e in old_g.edges:
         if e[0].label == node:
             g.add_edge(new_node, e[1])
             g.remove_edge(*e)
@@ -24,8 +35,5 @@ def do(graph: Graph, node: str, value: float, label: str | None = None) -> Graph
             g.remove_edge(*e)
 
     g.remove_node(graph.get_node(node))
-
-
-    from IPython import embed; embed()
 
     return Graph(label, g)
