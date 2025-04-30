124e3531ade9bc6e20fb340acb9c135d2504a536
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-04-30 15:07:48 +0100

    do

--- src/causalprog/algorithms/do.py
+++ src/causalprog/algorithms/do.py
@@ -2,7 +2,7 @@
 
 import numpy.typing as npt
 
-from causalprog.graph import Graph
+from causalprog.graph import Graph, ParameterNode
 
 
 def do(graph: Graph, node: str, value: float, label: str | None = None) -> Graph:
@@ -10,8 +10,22 @@ def do(graph: Graph, node: str, value: float, label: str | None = None) -> Graph
         label = f"{graph.label}|do({node}={value})"
 
     g = graph._graph.copy()
-    return Graph(label, g)
+
+    new_node = ParameterNode(node, value=value)
+    g.add_node(new_node)
+
+    edges_to_remove = []
+    for e in graph._graph.edges:
+        if e[0].label == node:
+            g.add_edge(new_node, e[1])
+            g.remove_edge(*e)
+        if e[1].label == node:
+            g.add_edge(e[0], new_node)
+            g.remove_edge(*e)
+
+    g.remove_node(graph.get_node(node))
+
 
     from IPython import embed; embed()
 
-    return graph.copy()
+    return Graph(label, g)
