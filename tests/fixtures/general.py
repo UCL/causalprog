aadad8b64f3b6f909b83650d6dbc0ca1e3b1f03c
willGraham01 <1willgraham@gmail.com>
2025-08-07 09:32:43 +0100

    Add a raises_context helper

--- tests/fixtures/general.py
+++ tests/fixtures/general.py
@@ -1,5 +1,9 @@
+import re
+from collections.abc import Callable
+
 import jax.random as jrn
 import pytest
+from _pytest.python_api import RaisesContext
 
 
 @pytest.fixture
@@ -10,3 +14,26 @@ def seed() -> int:
 @pytest.fixture
 def rng_key(seed: int):
     return jrn.key(seed)
+
+
+@pytest.fixture(scope="session")
+def raises_context() -> Callable[[Exception], RaisesContext]:
+    """Wrapper around `pytest.raises` that can be passed an Exception instance.
+
+    For a variable containing an `Exception` instance,
+
+    >>> expected_exception = Exception("message")
+
+    the following statements are equivalent:
+
+    >>> pytest.raises(
+    ...     type(expected_exception),
+    ...     match=re.escape(str(expected_exception))
+    ... )
+    >>> raises_context(expected_exception)
+    """
+
+    def _inner(match_error: Exception) -> RaisesContext:
+        return pytest.raises(type(match_error), match=re.escape(str(match_error)))
+
+    return _inner
