e89cdd863f7912ddadfc2b2e60b5371102f439cd
willGraham01 <1willgraham@gmail.com>
2025-09-25 09:50:40 +0100

    Fix test_parameter node since jnp.allclose can't handle lists

--- tests/test_graph/test_parameters.py
+++ tests/test_graph/test_parameters.py
@@ -1,15 +1,8 @@
 """Tests for graph module."""
 
-from typing import Literal, TypeAlias
-
 import jax.numpy as jnp
 
-from causalprog.graph import DistributionNode, ParameterNode
-
-NormalGraphNodeNames: TypeAlias = Literal["mean", "cov", "X"]
-NormalGraphNodes: TypeAlias = dict[
-    NormalGraphNodeNames, DistributionNode | ParameterNode
-]
+from causalprog.graph import ParameterNode
 
 
 def test_parameter_node(rng_key, raises_context):
@@ -19,5 +12,5 @@ def test_parameter_node(rng_key, raises_context):
         node.sample({}, {}, 1, rng_key=rng_key)
 
     assert jnp.allclose(
-        node.sample({node.label: 0.3}, {}, 10, rng_key=rng_key)[0], [0.3] * 10
+        node.sample({node.label: 0.3}, {}, 10, rng_key=rng_key), jnp.full((10,), 0.3)
     )
