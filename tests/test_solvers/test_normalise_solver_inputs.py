cf832c416a432fdf1b2acf3378c03de8e9d3a570
Sam Molyneux <samjmolyneux@gmail.com>
2025-11-01 02:56:37 +0000

    remove factories in favour of callback placeholder

--- tests/test_solvers/test_normalise_solver_inputs.py
+++ tests/test_solvers/test_normalise_solver_inputs.py
@@ -1,57 +1,35 @@
-from collections.abc import Callable
-
 import pytest
 
 from causalprog.solvers.iteration_result import IterationResult
 from causalprog.solvers.solver_callbacks import _normalise_callbacks
 
 
+def _cb(ir: IterationResult) -> None:
+    """Placeholder callback"""
+
+
 @pytest.mark.parametrize(
-    (
-        "make_input_callbacks",
-        "make_expected_output_callbacks",
-    ),
+    ("func_input", "expected"),
     [
-        (
-            lambda cb: cb,
-            lambda cb: [cb],
-        ),
-        (
-            lambda cb: [cb, cb],
-            lambda cb: [cb, cb],
-        ),
-        (
-            lambda cb: None,  # noqa: ARG005
-            lambda cb: [],  # noqa: ARG005
-        ),
-        (
-            lambda cb: [],  # noqa: ARG005
-            lambda cb: [],  # noqa: ARG005
-        ),
+        (_cb, [_cb]),
+        ([_cb, _cb], [_cb, _cb]),
+        (None, []),
+        ([], []),
+        (42, TypeError("'int' object is not iterable")),
     ],
     ids=[
         "single callable",
         "list of two callables",
         "callbacks=None",
         "callbacks=[]",
+        "callbacks=42",
     ],
 )
-def test_normalise_callbacks(
-    make_input_callbacks: Callable,
-    make_expected_output_callbacks: Callable,
-) -> None:
+def test_normalise_callbacks(func_input, expected, raises_context) -> None:
     """Test that callbacks are normalised correctly."""
 
-    def callback(iter_result: IterationResult) -> None:
-        pass
-
-    input_callbacks = make_input_callbacks(callback)
-    expected_output_callbacks = make_expected_output_callbacks(callback)
-
-    assert _normalise_callbacks(input_callbacks) == expected_output_callbacks
-
-
-def test_normalise_invalid_callbacks(raises_context) -> None:
-    """Test that invalid callbacks raise TypeError."""
-    with raises_context(TypeError("'int' object is not iterable")):
-        _normalise_callbacks(42)  # type: ignore[arg-type]
+    if isinstance(expected, Exception):
+        with raises_context(expected):
+            _normalise_callbacks(func_input)
+    else:
+        assert _normalise_callbacks(func_input) == expected
