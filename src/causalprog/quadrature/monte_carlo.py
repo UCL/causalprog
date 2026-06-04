7110359a9efc203c5cff31b9374538a14dc86644
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-06-04 15:43:43 +0100

    add monte cargo quadrature

---
+++ src/causalprog/quadrature/monte_carlo.py
@@ -0,0 +1,29 @@
+"""Monte Carlo quadrature."""
+
+
+import jax
+import numpy as np
+import numpy.typing as npt
+
+from .base import QuadratureMethod
+
+
+class MonteCarloGaussianQuadrature(QuadratureMethod):
+    """Monte Carlo quadrature sampled from standard Gaussian."""
+
+    def __init__(self, npoints: int, *, rng_key: jax.Array):
+        """
+        Initialise.
+
+        Args:
+            npoints: The number of quadrature points
+
+        """
+        super().__init__(npoints)
+        self.rng_key = rng_key
+
+    def points_and_weights(self) -> tuple[npt.NDArray, npt.NDArray]:
+        """Get the quadrature points and weights."""
+        return jax.random.normal(self.rng_key, (self.npoints,)), np.ones(
+            self.npoints
+        ) / self.npoints
