b371c0b0c6107f7340f169995c0050deb820adb2
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-25 10:38:14 +0100

    Add pts / wts tuple listing function, and enforce integral rules

--- src/causalprog/quadrature/base.py
+++ src/causalprog/quadrature/base.py
@@ -1,8 +1,13 @@
 """Base quadrature class."""
 
 from abc import ABC, abstractmethod
+from collections.abc import Callable
+from typing import Concatenate, ParamSpec, TypeAlias
 
 import numpy.typing as npt
+
+IntegrandArgs = ParamSpec("IntegrandArgs")
+Integrand: TypeAlias = Callable[Concatenate[float, IntegrandArgs], float]
 
 
 class QuadratureMethod(ABC):
@@ -24,5 +29,24 @@ class QuadratureMethod(ABC):
         return self._npts
 
     @abstractmethod
+    def integrate(
+        self,
+        integrand: Integrand,
+        a: float = -1.0,
+        b: float = 1.0,
+        *integrand_args: IntegrandArgs.args,
+        **integrand_kwargs: IntegrandArgs.kwargs,
+    ) -> float:
+        """
+        Integrate the `integrand` over `[a,b]` using the `QuadratureMethod`.
+
+        Subclasses should implement specific details.
+        """
+
+    @abstractmethod
     def points_and_weights(self) -> tuple[npt.NDArray, npt.NDArray]:
         """Get the quadrature points and weights."""
+
+    def pts_wts_tuples(self) -> list[tuple[float, float]]:
+        """Get `(point, weight)` pairs as a list of tuples."""
+        return list(zip(*self.points_and_weights(), strict=True))
