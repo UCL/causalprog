b116437d652d779890004f0232c407c2cb5b4e66
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-25 11:58:24 +0100

    Morph Gaussian qaud to generate points & weights for a given interval

--- src/causalprog/quadrature/base.py
+++ src/causalprog/quadrature/base.py
@@ -53,9 +53,13 @@ class QuadratureMethod(ABC):
         """
 
     @abstractmethod
-    def points_and_weights(self) -> tuple[npt.NDArray, npt.NDArray]:
-        """Get the quadrature points and weights."""
+    def points_and_weights(
+        self, a: float = -1.0, b: float = 1.0
+    ) -> tuple[npt.NDArray, npt.NDArray]:
+        """Get quadrature points and weights for performing integration on $[a,b]$."""
 
-    def pts_wts_tuples(self) -> list[tuple[float, float]]:
+    def pts_wts_tuples(
+        self, a: float = -1.0, b: float = 1.0
+    ) -> list[tuple[float, float]]:
         """Get `(point, weight)` pairs as a list of tuples."""
-        return list(zip(*self.points_and_weights(), strict=True))
+        return list(zip(*self.points_and_weights(a=a, b=b), strict=True))
