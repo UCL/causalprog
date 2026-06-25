2815aa541aea3596c02d35e63519a989fc810c1b
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-25 11:05:30 +0100

    Gaussian quadrature test suite spans change of interval + exact polynomials

--- tests/test_quadrature/test_gaussian_quadrature.py
+++ tests/test_quadrature/test_gaussian_quadrature.py
@@ -1,17 +1,52 @@
+import jax
 import jax.numpy as jnp
+import pytest
 
 from causalprog.quadrature import GaussianQuadrature
 
 
+@pytest.mark.parametrize(
+    ("coeffs", "interval", "exact_integral"),
+    [
+        pytest.param(
+            jnp.array([1.0]),
+            (-1, 1),
+            2.0,
+            id="[-1, 1] f(x) = 1",
+        ),
+        pytest.param(
+            jnp.array([0.0, 1.0]),
+            (-1, 1),
+            0.0,
+            id="[-1, 1] f(x) = x",
+        ),
+        pytest.param(
+            jnp.array([1.0, -2.0, 1.0]),
+            (-1, 1),
+            8.0 / 3.0,
+            id="[-1, 1] f(x) = x^2 - 2x + 1",
+        ),
+        pytest.param(
+            jnp.array([0.0, 1.0]),
+            (0, 5),
+            5.0 * 5.0 / 2.0,
+            id="[0, 5] f(x) = x",
+        ),
+        pytest.param(
+            jnp.array([0.0, 1.0]),
+            (-5, 0),
+            -5.0 * 5.0 / 2.0,
+            id="[-5, 0] f(x) = x",
+        ),
+    ],
+)
 def test_gaussian_quadrature_polynomials(
-    coeffs: jnp.Array, interval: tuple[float, float], exact_integral: float
+    coeffs: jax.Array, interval: tuple[float, float], exact_integral: float
 ) -> None:
-    degree = coeffs.size - 1
-
-    def _integrand(x: float, c: jnp.Array):
+    def _integrand(x: float, c: jax.Array):
         return (c * x ** jnp.arange(c.size)).sum()
 
-    computed_integral = GaussianQuadrature(degree).integrate(
+    computed_integral = GaussianQuadrature(coeffs.size).integrate(
         _integrand, a=interval[0], b=interval[1], c=coeffs
     )
 
