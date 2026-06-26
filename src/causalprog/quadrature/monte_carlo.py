5ea8be5ea5b00d86ffb43f65fd60b384cfa69e2f
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-26 09:43:59 +0100

    Write MC integration with uniform weights

--- src/causalprog/quadrature/monte_carlo.py
+++ src/causalprog/quadrature/monte_carlo.py
@@ -2,6 +2,7 @@
 
 import jax
 import numpy.typing as npt
+from typing_extensions import override
 
 from .base import Integrand, IntegrandArgs, RNGQuadratureMethod
 
@@ -37,12 +38,54 @@ class MonteCarloGaussianQuadrature(RNGQuadratureMethod):
 
         return result / self.n_points
 
+    @override
     def points_and_weights(
         self, a: float = -1.0, b: float = 1.0
     ) -> tuple[npt.NDArray, npt.NDArray]:
-        """Get the quadrature points and weights."""
         pts = jax.random.truncated_normal(
             self.rng_key, lower=a, upper=b, shape=(self.n_points,)
         )
         wts = jax.scipy.stats.truncnorm.pdf(pts, a, b)
+        return pts, wts
+
+
+class UniformWeightGaussianSampleMonteCarloQuadrature(RNGQuadratureMethod):
+    r"""
+    Monte Carlo quadrature, sampled from a standard Gaussian, but using uniform weights.
+
+    Let $N$ be the number of sample points to be used by the scheme.
+    The quadrature method approximates the integral
+
+    $$
+    \int_a^b f(x) dx
+    \approx \frac{1}{N}\sum_{p_i}\frac{f(p_i)},
+    $$
+
+    where $p_i\in[a,b]$ are $N$ samples drawn from a standard Gaussian.
+    """
+
+    def integrate(
+        self,
+        integrand: Integrand,
+        a: float = -1.0,
+        b: float = 1.0,
+        *integrand_args: IntegrandArgs.args,
+        **integrand_kwargs: IntegrandArgs.kwargs,
+    ) -> float:
+        """Perform Monte-Carlo integration of the `integrand` over $[a,b]$."""
+        pts, _ = self.points_and_weights(a=a, b=b)
+        ptwise_evaluation = jax.numpy.apply_along_axis(
+            integrand, 0, pts, *integrand_args, **integrand_kwargs
+        )
+
+        return ptwise_evaluation.sum() / self.n_points
+
+    @override
+    def points_and_weights(
+        self, a: float = -1.0, b: float = 1.0
+    ) -> tuple[npt.NDArray, npt.NDArray]:
+        pts = jax.random.truncated_normal(
+            self.rng_key, lower=a, upper=b, shape=(self.n_points,)
+        )
+        wts = jax.numpy.full((self.n_points,), 1.0 / self.n_points)
         return pts, wts
