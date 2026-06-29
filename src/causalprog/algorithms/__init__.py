5cf59ecbf1c99cd65bb8c33e12d4c223e6063b13
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-06-29 14:22:43 +0100

    RUFF

--- src/causalprog/algorithms/__init__.py
+++ src/causalprog/algorithms/__init__.py
@@ -7,8 +7,8 @@ from .moments import expectation, moment, standard_deviation
 __all__ = (
     "do",
     "evaluate",
+    "evaluate_down_to",
     "expectation",
     "moment",
     "standard_deviation",
-    "evaluate_down_to",
 )
