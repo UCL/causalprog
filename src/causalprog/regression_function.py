2a01e388ca6e66cf29885b604fdc1d2ace33c91d
willGraham01 <ccaegra@ucl.ac.uk>
2026-06-29 10:47:46 +0100

    Begin process of using actual graph attributes in place of placeholders

--- src/causalprog/regression_function.py
+++ src/causalprog/regression_function.py
@@ -6,7 +6,7 @@ from jax.nn import sigmoid, softmax, tanh
 from jax.numpy.linalg import norm
 from numpy.typing import NDArray
 
-from .graph import Graph
+from .graph import ContinuousRandomVariableNode, DiscreteRandomVariableNode, Graph
 from .quadrature.base import QuadratureMethod
 
 
@@ -28,19 +28,24 @@ def build_regression_function(
 
     where $s_q, w_q$ are sample points drawn from a quadrature rule.
     """
+    node_c: DiscreteRandomVariableNode = graph.get_node("C")
+    node_uy: ContinuousRandomVariableNode = graph.get_node("UY")
+    node_ux: ContinuousRandomVariableNode = graph.get_node("UX")
+    node_x: ContinuousRandomVariableNode = graph.get_node("X")
+    node_y: ContinuousRandomVariableNode = graph.get_node("Y")
     # TODO: Need to know where these attributes are stored on the graph nodes.
     # In particular, these f_r and f_m aren't actually going to be at the _func
     # attribute, since that's going to be \f_pi??
     # Also, we could maybe store sigma_czl on $U_Y$ itself, to save assembling it
     # here?
-    f_m = graph.get_node("U_Y").f_m
-    f_r = graph.get_node("U_Y").f_r
-    f_pi = graph.get_node("U_Y").f_pi  # would be replaced with graph.evaluate("C", ...)
+    f_m = node_uy.f_m
+    f_r = node_uy.f_r
+    f_pi = node_uy.f_pi  # would be replaced with graph.evaluate("C", ...)
     # Should be using graph.evaluate here!
     # Though given that we'll also want to compute the intermediate values to compute
     # the _v_y, _m_y, etc, it would be ideal if evaluate returned a dict of all nodes
     # that were evaluated...!
-    f_y = graph.get_node("Y").f_y  # would be replaced with graph.evaluate("Y", ...)
+    f_y = node_y.f_y  # would be replaced with graph.evaluate("Y", ...)
 
     # Need an inverse step here too? So whatever normalising flow thing is attached
     # to X, it will also need a back-propagation method too?
@@ -49,10 +54,9 @@ def build_regression_function(
     # other times when we just want to compute the value of X from u, z, l... but then
     # a two-way edge means we don't have a DAG
     def g(x: float, u: float, l: float) -> NDArray:
-        return graph.get_node("X").f_inverse(x, u, l, trained_parameters["theta_x"])
+        return node_x.f_inverse(x, u, l, trained_parameters["theta_x"])
 
-    # Need to be able to extract the unique values of the indicator C
-    c_values = graph.get_node("C").discrete_values
+    c_values = node_c.possible_values
 
     # TODO: c, z, l are all floats... but this is vectorizable potentially?
     def _sigma(
@@ -85,23 +89,23 @@ def build_regression_function(
     ) -> NDArray:
         return _sigma(c, z, l, theta_m, theta_r) * g(x, z, l)
 
+    def _integrand(s_q, z, l, x, theta):
+        u = g(x, z, l)
+        result = 0.0
+
+        for c in c_values:
+            result += _pi_ul(c, u, l, theta["theta_pi"]) * f_y(
+                s_q * _v_y(c, z, l, theta["theta_m"])
+                + _m_y(c, z, l, x, theta["theta_m"], theta["theta_r"]),
+                x,
+                l,
+            )
+        return result
+
     def _r(theta: dict[str, NDArray], x: float, z: float, l: float) -> NDArray:
         """"""
-        u = g(x, z, l)
-
-        # Naive implementation that I know works
-        result = 0.0
-        for w_q, s_q in zip(*quadrature.points_and_weights(), strict=True):
-            inner_sum = 0.0
-            for c in c_values:
-                inner_sum += _pi_ul(c, u, l, theta["theta_pi"]) * f_y(
-                    s_q * _v_y(c, z, l, theta["theta_m"])
-                    + _m_y(c, z, l, x, theta["theta_m"], theta["theta_r"]),
-                    x,
-                    l,
-                )
-            result += w_q * inner_sum
-
-        return result
+        return quadrature.integrate(
+            _integrand, a=-float("inf"), b=float("inf"), z=z, l=l, x=x, theta=theta
+        )
 
     return _r
