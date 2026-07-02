cc4cf75ce306f1d31884f84b28b48bbf4efdf3b9
willGraham01 <ccaegra@ucl.ac.uk>
2026-07-02 10:06:32 +0100

    Typehinting should work now, except on evaluate

--- src/causalprog/regression_function.py
+++ src/causalprog/regression_function.py
@@ -10,9 +10,8 @@ from numpy.typing import NDArray
 from .graph import ContinuousRandomVariableNode, DiscreteRandomVariableNode, Graph
 from .quadrature.base import QuadratureMethod
 
-MLPAlias: TypeAlias = Callable[
-    [dict[str, NDArray], dict[str, NDArray]], float | NDArray
-]
+ModelParam: TypeAlias = dict[str, NDArray]  # Should be dict[str, PyTree] I guess...
+MLPAlias: TypeAlias = Callable[[dict[str, NDArray], ModelParam], float | NDArray]
 
 
 def build_regression_function(
@@ -48,63 +47,51 @@ def build_regression_function(
 
     def f_x_inverse(xzl: dict[str, NDArray]) -> float | NDArray:
         r"""
-        $g(x, z, l) := f_X^{-1}(x, z, l).
+        $g(x, z, l) := f_X^{-1}(x, z, l; theta_x).
 
         At the time of evaluation, this is known (since we are given $\theta_X$).
         """
         return node_ux.evaluate(xzl, {"theta_X": theta_x})
 
-    def pi_ul(
-        ulc: dict[str, NDArray], model_params: dict[str, NDArray]
-    ) -> float | NDArray:
+    def pi_ul(ulc: dict[str, NDArray], theta_pi: ModelParam) -> float | NDArray:
         r"""$\pi_ul(c, u, l; theta_pi)."""
-        return node_uy.evaluate(ulc, {"theta_X": theta_x, **model_params})
+        return node_uy.evaluate(ulc, theta_pi)
 
-    def f_y(
-        node_values: dict[str, NDArray], model_params: dict[str, NDArray]
-    ) -> float | NDArray:
-        r"""$f_Y(c, z, l, x, u_y)."""
-        return node_y.evaluate(node_values, {"theta_X": theta_x, **model_params})
+    def f_y(x_uy: dict[str, NDArray], theta_y: ModelParam) -> float | NDArray:
+        r"""$f_Y(x, u_y; theta_y)."""
+        return node_y.evaluate(x_uy, theta_y)
 
     f_m: MLPAlias = node_uy.f_m
     f_r: MLPAlias = node_uy.f_r
     c_values = node_c.possible_values
 
     def _integrand(
-        s_q: float, xzl: dict[str, NDArray], model_params: dict[str, NDArray]
+        s_q: float, xzl: dict[str, NDArray], model_params: dict[str, ModelParam]
     ) -> float | NDArray:
         """Regression function integrand, usable with `QuadratureMethod.integrate`."""
         u = f_x_inverse(xzl)
 
         theta_m = model_params["theta_m"]
         theta_r = model_params["theta_r"]
+        theta_pi = model_params["theta_pi"]
+        theta_y = model_params["theta_y"]
+
         x = xzl["X"]
         z = xzl["Z"]
         el = xzl["L"]
 
         result = 0.0
         for c in c_values:
-            values = {"C": c, "Z": z, "L": el}
+            czl = {"C": c, "Z": z, "L": el}
 
-            f_r_vector = tanh(f_r(values, theta_r))
-            sigmoid_f_m = sigmoid(f_m(values, theta_m))
+            f_r_vector = tanh(f_r(czl, theta_r))
+            sigmoid_f_m = sigmoid(f_m(czl, theta_m))
             v_y = 1.0 - sigmoid_f_m**2
             m_y = u * sigmoid_f_m * f_r_vector / (norm(f_r_vector) ** 2)
+            u_y = s_q * v_y + m_y
 
-            pi_ul_prediction = pi_ul(
-                {"C": c, "L": el, "Z": z, "X": x, "U_X": u, **model_params},
-            )
-            f_y_prediction = f_y(
-                {
-                    "C": c,
-                    "L": el,
-                    "Z": z,
-                    "X": x,
-                    "U_X": u,
-                    "U_Y": s_q * v_y + m_y,
-                    **model_params,
-                },
-            )
+            pi_ul_prediction = pi_ul({"C": c, "L": el, "U_X": u}, theta_pi)
+            f_y_prediction = f_y({"X": x, "U_Y": u_y}, theta_y)
             result += pi_ul_prediction * f_y_prediction
         return result
 
