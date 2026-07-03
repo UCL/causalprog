0b4be2cb586ce614fa41cd8c9041dbb11f6fc635
willGraham01 <ccaegra@ucl.ac.uk>
2026-07-03 12:06:49 +0100

    Tidy up the test file so it actually explains what it does

--- tests/test_integration/test_ricardo_regression_function.py
+++ tests/test_integration/test_ricardo_regression_function.py
@@ -1,33 +1,52 @@
 import jax
 import jax.numpy as jnp
 
-from causalprog.graph.ricardo import build_regression_function, example_model
-from causalprog.quadrature import UniformWeightMonteCarloGaussianQuadrature as UWMCGQ
+from causalprog.graph.ricardo import MLPAlias, build_regression_function, example_model
+from causalprog.quadrature import UniformWeightMonteCarloGaussianQuadrature as UWMCGQuad
 
-# Need to write suitable compute functions!
-# Also need to reverse the x -> u_x edge too.
-# u_x needs a compute function.
-# u_y needs a compute function from c, u_x, and l (?)
-# y needs a compute function from u_y and x (and other vars?)
-# Nothing else _needs_ a compute function.
-# Then the u_y node needs some extra functions tacked onto it...
-# and some new nodes added as parents for f_r, f_m etc.
+# Note that x64 precision makes a big difference here, since numerical derivatives are
+# sensitive to "non-ops". EG Calculations that analytically make no difference to the
+# end result, but numerically _can_ affect the output value due to rounding etc.
+jax.config.update("jax_enable_x64", val=True)
 
 
-def test_what_am_i_doing(
-    rng_key, k_len: int = 5, z_len: int = 10, n_points: int = 10000
+def test_uy_independent(
+    rng_key,
+    k_len: int = 5,
+    z_len: int = 10,
+    n_points: int = 1000,
+    n_eval_pts_per_dim: int = 10,
+    f_r: MLPAlias = lambda czl, theta_m: theta_m * jnp.ones_like(czl["z"]),
+    f_m: MLPAlias = lambda czl, theta_r: czl["c"] * theta_r,
+    f_ux: MLPAlias = lambda xzl, theta_x: xzl["x"] * theta_x[0],
+    f_y: MLPAlias = lambda xu_y, theta_y: theta_y[0] * jnp.exp(-(xu_y["x"] ** 2)),
+    r_analytic: MLPAlias = lambda xzl, theta: theta["theta_y"][0]
+    * jnp.exp(-(xzl["x"] ** 2)),
 ) -> None:
-    def f_ux(xzl, theta_x):
-        return xzl["x"] * theta_x[0]
+    r"""Build a $U_Y$-independent regression function.
+
+    Under the assumption that $f_Y$ is independent of $U_Y$, the regression function
+    $r(x, z, l; \theta)$ should amount to integrating a constant (albeit one that is
+    parametrised by the remaining input arguments to $f_Y$).
+
+    Explicitly, in this test we set $f_Y(u_y, x, l; \theta_Y) = \theta_Y[0]e^{-x^2}$.
+    The other supporting functions are set to be non-trivial (but also unrealistic) just
+    so that we can check the auto-diff isn't making too much of a mess when we take
+    derivatives of variables that (in theory) $r$ doesn't depend on.
+
+    Note that increasing the value of `n_eval_pts_per_dim` comes with a massive memory
+    and time overhead - this dictates the size of the "grid" over which we are
+    evaluating the constructed $r$ and its derivative, and this grid is 7D!
+
+    On x32-only machines, the computed partial derivatives for non-$\theta_Y$ variables
+    are known to be of approximately 1e-7 size, which means that `jnp.allclose` will
+    flag them as "not being close" to 0. This appears to be a numerical-rounding error,
+    since enabling x64-precision calculations makes this issue disappear.
+    """
 
     def f_pi(ucl, theta_pi):
-        return jnp.ones((k_len,))
-
-    def f_y(xu_y, theta_y):
-        return theta_y[0] * jnp.exp(-(xu_y["x"] ** 2))
-
-    f_r = lambda *args, **kwargs: jnp.ones((z_len,))
-    f_m = lambda *args, **kwargs: -float("inf")
+        """Needs to return a `k_len` vector, so must be defined inside the test body."""
+        return theta_pi * ucl["c"] + ucl["u_x"] * jnp.arange(k_len)
 
     g = example_model(
         k=k_len,
@@ -38,22 +57,18 @@ def test_what_am_i_doing(
         compute_x=None,
         compute_y=f_y,
     )
-    # Manually attach methods to node for now
+    # Manually attach methods to node for now.FIXME: should be removed once we have
+    # a more elegant solution for attaching additional functions to nodes.
     g.get_node("u_y").f_r = f_r
     g.get_node("u_y").f_m = f_m
 
     given_theta_x = jnp.ones((1,))
-    r = build_regression_function(g, given_theta_x, UWMCGQ(n_points, rng_key=rng_key))
+    r = build_regression_function(
+        g, given_theta_x, UWMCGQuad(n_points, rng_key=rng_key)
+    )
     dr = jax.grad(r, argnums=1)
 
-    # In this setup, we should have
-    # r(x, z, l; theta) = theta_y[0]e^{-x^2} and thus
-    # dr(x, z, l; theta)/dtheta = 0 except for the theta_y component, which should be
-    # dr(x, z, l; theta)/dtheta_y = e^{-x^2}
-    r_analytic = lambda xzl, theta: theta["theta_y"][0] * jnp.exp(-(xzl["x"] ** 2))
-
     # Create "grid" across which to evaluate the built functions
-    n_eval_pts_per_dim = 10
     theta = {
         "theta_m": jnp.linspace(-1.0, 1.0, num=n_eval_pts_per_dim),
         "theta_r": jnp.linspace(-1.0, 1.0, num=n_eval_pts_per_dim),
@@ -93,15 +108,15 @@ def test_what_am_i_doing(
             ),
         )
 
-    # Evaluate the vectorised functions to check if they really do match?
     assert jnp.allclose(r(xzl, theta), r_analytic(xzl, theta))
-    # Check gradient computations too
-    dr_evaluation = dr(xzl, theta)
 
+    dr_evaluation = dr(xzl, theta)
     for key in theta:
         if key != "theta_y":
             # f_Y has been constructed to only depend on theta_Y
-            assert jnp.allclose(dr_evaluation[key], 0.0)
+            assert jnp.allclose(dr_evaluation[key], 0.0), (
+                f"{key}-derivative not close to 0 (max {dr_evaluation[key].max()})"
+            )
         else:
             # theta_y is the "last" key that gets vectorised
             computed_theta_y_deriv = dr_evaluation[key][..., -1]
