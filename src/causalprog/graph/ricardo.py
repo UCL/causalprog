e73a5801b7b6cff2433cd0cc0b5d62b1c15ac12b
willGraham01 <ccaegra@ucl.ac.uk>
2026-07-14 14:04:46 +0100

    Fix docstring name

--- src/causalprog/graph/ricardo.py
+++ src/causalprog/graph/ricardo.py
@@ -259,7 +259,7 @@ def learn_initialiser(
     Args:
         r: Regression function, $r$. Typically the output of `build_regression_function`
         evaluation_points: Set of evaluation points, $\mathcal{D}$
-        r_hat_pts: The values of the estimate of r at the evaluation points, $\hat{r}_i$
+        r_hat_i: The values of the estimate of r at the evaluation points, $\hat{r}_i$
         evaluation_points_axes_mapping: Axes to vectorising over when evaluating $r$
             at the `evaluation_points`.
         optimiser: Minimisation method, defined as a Python callable. It should accept
