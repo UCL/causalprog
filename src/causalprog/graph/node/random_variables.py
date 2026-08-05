ec0456811ee6a0030573896427bdb6148422df30
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2026-08-05 11:00:57 +0100

    use collections.abc.Callable over typing.Callable (#193)
    * use collections.abc.Callable over typing.Callable
    
    * ruff

--- src/causalprog/graph/node/random_variables.py
+++ src/causalprog/graph/node/random_variables.py
@@ -1,6 +1,6 @@
 """Graph nodes representing random variables."""
 
-import typing
+from collections.abc import Callable
 
 import jax
 import numpy as np
@@ -37,7 +37,7 @@ class RandomVariableNode(Node):
         *,
         shape: tuple[int, ...] = (),
         label: str,
-        compute: typing.Callable | None = None,
+        compute: Callable | None = None,
         parents: list[str] | None = None,
     ) -> None:
         """
@@ -119,7 +119,7 @@ class DiscreteRandomVariableNode(RandomVariableNode):
         values: list[float] | list[npt.NDArray[float]],
         shape: tuple[int, ...] = (),
         label: str,
-        compute: typing.Callable | None = None,
+        compute: Callable | None = None,
     ) -> None:
         """
         Initialise.
