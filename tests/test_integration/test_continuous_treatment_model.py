b7ef831a8d54fed5bcaffb3ccfc2360417b9f6c9
willGraham01 <ccaegra@ucl.ac.uk>
2026-08-05 12:37:44 +0100

    Combine all loose treatment model tests

--- tests/test_integration/test_continuous_treatment_model.py
+++ tests/test_integration/test_continuous_treatment_model.py
@@ -5,7 +5,30 @@ from causalprog.graph import ContinuousRandomVariableNode
 from causalprog.graph.continuous_treatment import continuous_treatment_model
 
 
-def test_example_model_update():
+def test_treatment_model():
+    graph = continuous_treatment_model(
+        compute_u_x=lambda _data: 1.0,
+        compute_u_y=lambda _data: 1.0,
+        compute_phi_x=lambda _data: 1.0,
+        compute_x=lambda _data: 1.0,
+        compute_y=lambda _data: 1.0,
+    )
+    assert len(graph.nodes) == 8
+    assert len(graph.edges) == 8
+    edges = {(e[0].label, e[1].label) for e in graph.edges}
+    assert edges == {
+        ("l", "phi_x"),
+        ("c", "u_y"),
+        ("c", "u_x"),
+        ("u_x", "x"),
+        ("phi_x", "x"),
+        ("z", "x"),
+        ("u_y", "y"),
+        ("x", "y"),
+    }
+
+
+def test_treatment_model_update():
     """Test ust of replace_node to reverse an edge."""
     graph = continuous_treatment_model(
         compute_u_x=lambda _data: 1.0,
